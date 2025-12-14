92c413fbd0282f19e59087dd36da1190b6900db2
audh25-lab <audh25@gmail.com>
2025-12-14 16:07:10 +0500

    Create HistoryPortals.ts

---
+++ xr/HistoryPortals.ts
@@ -0,0 +1,11 @@
+export type HistoryPortal = {
+  country: string
+  era: string
+  entryPhrase: string
+}
+
+export const HistoryPortals: HistoryPortal[] = [
+  { country: "Egypt", era: "Ancient", entryPhrase: "Walk into the pyramids" },
+  { country: "Japan", era: "Edo", entryPhrase: "Enter the silent streets" },
+  { country: "Maldives", era: "1153 AD", entryPhrase: "Witness the arrival of Islam" }
+]
