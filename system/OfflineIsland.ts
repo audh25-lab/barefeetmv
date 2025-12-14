a9f095f058a0888867b96dfba97d186a55b869fe
audh25-lab <audh25@gmail.com>
2025-12-14 16:12:18 +0500

    Update OfflineIsland.ts

--- system/OfflineIsland.ts
+++ system/OfflineIsland.ts
@@ -1,1 +1,15 @@
-system/OfflineIsland.ts
+export class OfflineIsland {
+  private cache: Record<string, any> = {}
+
+  store(key: string, value: any) {
+    this.cache[key] = value
+    localStorage.setItem("barefeet-island", JSON.stringify(this.cache))
+  }
+
+  load(key: string) {
+    const data = localStorage.getItem("barefeet-island")
+    if (!data) return null
+    this.cache = JSON.parse(data)
+    return this.cache[key]
+  }
+}
