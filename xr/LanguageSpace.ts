c636a85247ba10070be6e1e86163e720cda0df19
audh25-lab <audh25@gmail.com>
2025-12-14 16:08:11 +0500

    Create LanguageSpace.ts

---
+++ xr/LanguageSpace.ts
@@ -0,0 +1,11 @@
+export type LanguageZone = {
+  language: string
+  script: string
+  interaction: string
+}
+
+export const LanguageSpaces: LanguageZone[] = [
+  { language: "Dhivehi", script: "Thaana", interaction: "Flowing symbols" },
+  { language: "Arabic", script: "Arabic", interaction: "Calligraphic motion" },
+  { language: "Japanese", script: "Kanji", interaction: "Spatial meaning blocks" }
+]
