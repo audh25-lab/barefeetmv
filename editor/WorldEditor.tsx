3e617c9f63f2bb4cd7e6c79961e034461f140d29
audh25-lab <audh25@gmail.com>
2025-12-16 03:37:02 +0500

    Update WorldEditor.tsx

--- editor/WorldEditor.tsx
+++ editor/WorldEditor.tsx
@@ -1,1 +1,17 @@
-editor/WorldEditor.tsx
+"use client"
+export default function WorldEditor({ world }: any) {
+  return (
+    <div className="fixed top-4 right-4 bg-black/70 p-3">
+      <button onClick={() => world.city.grow()}>
+        Grow City
+      </button>
+      <button onClick={() =>
+        world.politics.propose("Tax Cut", w =>
+          w.economy.earn("gold", 5)
+        )
+      }>
+        New Law
+      </button>
+    </div>
+  )
+}
