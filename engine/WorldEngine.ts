865d25dd99ecb6458e04e0a2a259ecbf6cb06c21
audh25-lab <audh25@gmail.com>
2025-12-16 01:49:33 +0500

    Update WorldEngine.ts

--- engine/WorldEngine.ts
+++ engine/WorldEngine.ts
@@ -1,14 +1,23 @@
 import * as THREE from "three"
+import { CompanionAgent } from "./CompanionAgent"
+import { loadIsland } from "./IslandWorld"
+import { enableXR } from "./XRMode"
 
 export class WorldEngine {
   scene = new THREE.Scene()
-  camera = new THREE.PerspectiveCamera(60, 1, 0.1, 1000)
+  camera = new THREE.PerspectiveCamera(65, 1, 0.1, 100)
   renderer = new THREE.WebGLRenderer({ antialias: true })
   clock = new THREE.Clock()
 
+  companion!: CompanionAgent
+
   constructor() {
-    this.camera.position.set(0, 1.6, 4)
     this.scene.background = new THREE.Color("#00111a")
+    this.camera.position.set(0, 1.6, 3)
+
+    enableXR(this.renderer)
+    loadIsland(this.scene, "Baa Atoll")
+    this.companion = new CompanionAgent(this.scene)
   }
 
   mount(dom: HTMLElement) {
@@ -19,12 +28,8 @@ export class WorldEngine {
 
   animate = () => {
     requestAnimationFrame(this.animate)
-    const dt = this.clock.getDelta()
-    this.update(dt)
+    const t = this.clock.getElapsedTime()
+    this.companion.update(t)
     this.renderer.render(this.scene, this.camera)
-  }
-
-  update(dt: number) {
-    // world ticks live here
   }
 }
